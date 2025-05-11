a41a3cc67778c6da31d92525beeaf35fca3d50ca
Igor Balos <ibalosh@gmail.com>
2025-05-11 08:38:39 +0200

    simplify controller

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -3,13 +3,10 @@ class ImportsController < ApplicationController
     @imports = Import.where(merchant_id: current_user.id).order(created_at: :desc).page(params[:page]).per(10)
   end
   def create
-    file = ActiveStorage::Blob.find_signed(uploaded_file)
-    if file.byte_size > 10.megabytes
-      redirect_to new_import_path, alert: "File is too large. Please upload a file smaller than 10MB."
-      return
-    end
+    blob = find_uploaded_file_blob
+    return if blob.nil? || reject_if_file_too_large(blob)
 
-    import = Import.new(merchant_id: current_user.id).tap { |import| import.file.attach(uploaded_file) }
+    import = build_import(blob)
 
     if import.save
       AffiliatesImportProcessingJob.perform_later(import, current_user.id)
@@ -21,6 +18,32 @@ class ImportsController < ApplicationController
   end
 
   private
+
+  UPLOADED_FILE_SIZE_LIMIT = 10.megabytes
+  UPLOADED_FILE_SIZE_LIMIT_IN_WORDS = ActiveSupport::NumberHelper.number_to_human_size(UPLOADED_FILE_SIZE_LIMIT)
+
+  def build_import(blob)
+    Import.new(merchant_id: current_user.id).tap do |import|
+      import.file.attach(blob)
+    end
+  end
+
+  def find_uploaded_file_blob
+    ActiveStorage::Blob.find_signed(uploaded_file)
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to new_import_path, alert: "Invalid file upload."
+    nil
+  end
+
+  def reject_if_file_too_large(blob)
+    return false if blob.byte_size <= UPLOADED_FILE_SIZE_LIMIT
+
+    blob.purge
+    redirect_to new_import_path, alert: "File is too large. " \
+      "Please upload a file smaller than #{UPLOADED_FILE_SIZE_LIMIT_IN_WORDS}."
+    true
+  end
+
   def uploaded_file
     params.require(:file)
   end
