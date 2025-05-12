b86015292d6722baa9caccebaace4929fe0b9395
Igor Balos <ibalosh@gmail.com>
2025-05-12 15:19:10 +0200

    improved copy

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -2,6 +2,7 @@ module NavbarHelper
   def role_span(type)
     color = (type == "admin") ? "bg-info" : "bg-warning"
     class_style = "badge #{color} text-white ms-2 bg-gradient"
-    content_tag(:span, "#{type.downcase} user", class: class_style)
+    label = type == "admin" ? "backdoor admin user" : "regular user"
+    content_tag(:span, label, class: class_style)
   end
 end
