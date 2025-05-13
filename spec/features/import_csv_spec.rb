57f535551b4e2ace9ace1e359f69e4ff1c58f1e2
Igor Balos <ibalosh@gmail.com>
2025-05-13 09:41:58 +0200

    added test for verifying that uploaded file is too large

--- spec/features/import_csv_spec.rb
+++ spec/features/import_csv_spec.rb
@@ -3,10 +3,6 @@ require "rails_helper"
 feature "CSV Import", type: :feature, async: false do
   let(:merchant) { create(:merchant) }
 
-  before do
-    allow_any_instance_of(ImportsController).to receive(:uploaded_file_is_valid).and_return(true)
-  end
-
   before(:each) do
     visit sessions_new_path
     fill_in "Slug", with: merchant.slug
@@ -14,7 +10,20 @@ feature "CSV Import", type: :feature, async: false do
     click_button "Sign In"
   end
 
-  it "User uploads a CSV files and sees it in history" do
+  it "Rejects file upload if file is too large" do
+    uploaded_file_double = instance_double(ActiveStorage::Blob, byte_size: 20.megabytes, purge: true)
+    allow(ActiveStorage::Blob).to receive(:find_signed).and_return(uploaded_file_double)
+    file_to_upload = create_temp_file(filename: "test.csv", content: "CONTENT")
+
+    visit new_import_path
+    attach_file("file", file_to_upload.path)
+    click_button "Upload and Import"
+    expect(page).to have_content("File is too large")
+  end
+
+  it "Uploads a CSV files and sees it in history" do
+    allow_any_instance_of(ImportsController).to receive(:uploaded_file_is_valid).and_return(true)
+
     content = <<~CSV
       merchant_slug,first_name,last_name,email,website_url,commissions_total
       merchant-a,John,Doe,john@example.com,https://example.com,100.50
